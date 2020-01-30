7fd0a747a8ff5170adddd97631026335f504e87f
Shota Ioramashvili <shotaioramashvili@gmail.com>
2020-01-30 12:45:07 +0400

    Initial Commit

---
+++ SharedFramework/SharedFramework.h
@@ -0,0 +1,19 @@
+//
+//  SharedFramework.h
+//  SharedFramework
+//
+//  Created by Shota Ioramashvili on 1/30/20.
+//  Copyright © 2020 shota.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SharedFramework.
+FOUNDATION_EXPORT double SharedFrameworkVersionNumber;
+
+//! Project version string for SharedFramework.
+FOUNDATION_EXPORT const unsigned char SharedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedFramework/PublicHeader.h>
+
+
